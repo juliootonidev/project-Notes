de486fdafd4d668ffd40b7c389a69cf215d87648
Julio Gabriel <julio.otoni@outlook.com>
2023-02-16 23:54:49 -0300

    Update

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const app = express();
 
 app.set('view engine','ejs');
 
+const blogs = [
+    {title: 'Sobre Javascritp', snippet:'lLorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis'},
+    {title: 'Sobre React.js', snippet:'lLorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis'},
+    {title: 'Sobre Node.js', snippet:'lLorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis'},
+]
+
 app.get('/',(req, res)=> {
-    res.render('index')
+    res.render('index', {title: 'Index', blogs})
 });
     
 app.get('/about',(req, res)=> {
-    res.render('about')
+    res.render('about', {title: 'About'})
+});
+
+app.get('/blogs/create',(req, res)=> {
+    res.render('create', {title: 'Create'})
 });
 
 app.use('/',(req, res)=> {
-    res.render('404')
+    res.render('404',{title: 'Not found'})
 });
 
 app.listen(3000, ()=>{
